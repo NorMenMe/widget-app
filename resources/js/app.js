9d5f78930f787735cd366a2928af68b3f44b4d76
nmetz <info@normanmetz.com>
2025-11-17 10:58:12 +0100

    feat(icon): add icon into folder atoms

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import Icon from './Components/Icon.vue';
+import Icon from './Components/Atoms/Icon.vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
