dae854bd0f0efa4f58493806fe92b34c9dc11b88
nmetz <info@normanmetz.com>
2025-11-17 11:04:46 +0100

    feat(dashboard): add organism dashboard

---
+++ resources/js/Components/Organism/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="dashboard">
+        <div class="dashboard__inner">
+            <div class="dashboard__main">
+                <search-field></search-field>
+                <teaser-dashboard :data="data"></teaser-dashboard>
+            </div>
+            <div class="dashboard__extra"></div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import SearchField from '@/Components/Molecules/SearchField.vue';
+    import TeaserDashboard from '@/Components/Molecules/TeaserDashboard.vue';
+</script>
+
--- resources/js/Pages/StartPage.vue
+++ resources/js/Pages/StartPage.vue
@@ -1,17 +1,9 @@
 <template>
-    <div class="dashboard">
-        <div class="dashboard__main">
-            <search-field></search-field>
-            <teaser-dashboard :data="data"></teaser-dashboard>
-        </div>
-        <div class="dashboard__extra"></div>
-    </div>
+    <dashboard :data="data"></dashboard>
 </template>
 
 <script setup>
-    import SearchField from '../Components/Molecules/SearchField.vue';
-    import TeaserDashboard from '@/Components/Molecules/TeaserDashboard.vue';
-
+    import Dashboard from '@/Components/Organism/Dashboard.vue';
 
     defineProps({
         data: Array,
