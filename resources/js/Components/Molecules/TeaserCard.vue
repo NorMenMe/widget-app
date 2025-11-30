f6f4a020725a72b91b6cc9b7200e68c2144dcf16
nmetz <info@normanmetz.com>
2025-11-30 13:11:23 +0100

    feat(teaser-card): add teaser-card component & basic styles

---
+++ resources/js/Components/Molecules/TeaserCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <section class="teaser-card">
+        <article class="teaser-card__article">
+            <div class="front">
+                <h4>Sun</h4>
+                <icon
+                    class="teaser-card__icon"
+                    name="cloud"
+                    :name="currentWeatherIcon"
+                    ></icon>
+            </div>
+            <div class="back">
+                <p>
+                    Min:
+                    <span>7°</span>
+                </p>
+                <p>
+                    Max:
+                    <span>14°</span>
+                </p>
+            </div>
+        </article>
+    </section>
+</template>
+
+<script setup>
+import Icon from "@/Components/Atoms/Icon.vue";
+</script>
+
