264e080fc52b0e89a8312c6cf600af548221f4f5
nmetz <info@normanmetz.com>
2026-01-05 10:55:01 +0100

    feat(teaser-chat): add a teaser-chat component

---
+++ resources/js/Components/Molecules/TeaserChat.vue
@@ -0,0 +1,50 @@
+<template>
+    <section class="teaser-chat" :class="{'is-read' : isRead}">
+        <picture class="teaser-chat__image">
+            <img :src="item.image" alt="image of teaser-chat person">
+        </picture>
+        <div class="teaser-chat__content flow-xs">
+            <h3 class="teaser-chat__heading">{{ item.name }}</h3>
+            <p class="teaser-chat__copy">{{ item.copy }}</p>
+        </div>
+        <button
+            class="teaser-chat__button"
+            @click="toggleRead">
+            <icon class="teaser-chat__icon" name="unread"></icon>
+        </button>
+    </section>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+import Icon from '@/Components/Atoms/Icon.vue';
+
+const isRead = ref(false);
+
+const props = defineProps({
+	item: {
+		type: Object,
+		required: true,
+	},
+    forceRead: {
+		type: Boolean,
+		default: false,
+	},
+});
+
+function toggleRead() {
+    if (isRead.value) {
+        isRead.value = false
+        buttonText = ''
+    } else {
+        isRead.value = true;
+    }
+}
+
+watch(() => props.forceRead, (newValue) => {
+    if (props.forceRead) {
+        isRead.value = true;
+    }
+});
+</script>
+
