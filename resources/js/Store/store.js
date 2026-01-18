599d0142917a5f96016f4137bfaf595bdf331645
nmetz <info@normanmetz.com>
2026-01-18 11:13:28 +0100

    feat(store): spread hardcoded data from the file helpers

--- resources/js/Store/store.js
+++ resources/js/Store/store.js
@@ -3,14 +3,15 @@ import { chatItems } from '@/Components/Helpers/helpers.data-hardcoded';
 
 export const store = reactive({
     teaserChatItems : [],
-    generateChatItems() {
+    initializeChatItems() {
         const counter = chatItems.items.length -1;
         const key = 'isRead';
 
         for (let x = 0; x <= counter; x++) {
-          const object = {};
+          const object = {...chatItems.items[x]};
           object[key] = false;
           this.teaserChatItems.push(object);
         }
-    }
+    },
+    isAllChatRead : false,
 })
