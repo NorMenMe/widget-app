c33920d1475d92d2d6a0fd8a92d3a40a47c0adfc
nmetz <info@normanmetz.com>
2026-01-26 11:38:00 +0100

    feat(cards): remove on mobile redundant animation

--- resources/js/Components/Molecules/Cards.vue
+++ resources/js/Components/Molecules/Cards.vue
@@ -3,10 +3,6 @@
         <div class="cards__inner">
             <h2 class="cards__heading">The upcoming week</h2>
             <div class="cards__content">
-                <div class="cards__copy">
-                    <icon class="cards__icon" name="arrow-down"></icon>
-                    <p>Click on the card to read the temperature.</p>
-                </div>
                 <ul class="cards__list">
                     <li class="cards__item" v-for="(day, key) in forecastDays" :key="key">
                         <teaser-card :data="day"></teaser-card>
