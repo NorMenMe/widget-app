8f6cd2a0579289531898a6eb09225aac9301d1e9
nmetz <info@normanmetz.com>
2026-02-02 13:51:04 +0100

    feat(icon): add more spedific vite-svg-loader config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import svgLoader from 'vite-svg-loader'
+import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
     plugins: [
@@ -18,7 +18,9 @@ export default defineConfig({
                 },
             },
         }),
-        svgLoader(),
+        svgLoader({
+         defaultImport: 'component',
+        }),
     ],
     resolve: {
     alias: {
