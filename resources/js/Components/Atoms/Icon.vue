4cd2c784ee607d6c8953b6a69738b379dc7f7824
nmetz <info@normanmetz.com>
2026-02-02 14:11:43 +0100

    feat(icon): import icon as a string

--- resources/js/Components/Atoms/Icon.vue
+++ resources/js/Components/Atoms/Icon.vue
@@ -1,7 +1,7 @@
 <template>
-  <MyIcon />
+  <div v-html="MyIcon"></div>
 </template>
 
 <script setup>
-    import MyIcon from '@/Images/icons/sun.svg'
+    import MyIcon from '@/Images/icons/sun.svg?raw'
 </script>
