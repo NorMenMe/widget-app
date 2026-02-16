c1191bcbd9d3610e720aa0edf94adf742fe05673
nmetz <info@normanmetz.com>
2026-02-16 09:30:13 +0100

    feat(alert): add alert molecule

---
+++ resources/js/Components/Molecules/Alert.vue
@@ -0,0 +1,32 @@
+<template>
+  <div role="alert" :class="`alert ${alertType}`">
+    <span><slot></slot></span>
+    <button @click="close">
+        ⅹ
+        <span class="sr-only">close button</span>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'info'
+    }
+})
+
+const alertType = computed(() => {
+  return {
+    error: 'alert-error',
+  }[props.type]
+})
+
+const emit = defineEmits(['closed'])
+
+const close = () => {
+  emit('closed')
+}
+</script>
