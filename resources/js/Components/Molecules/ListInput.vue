cd780e3e5601b22709ffacb6d54a2b71f7d81480
nmetz <info@normanmetz.com>
2026-07-28 14:11:24 +0200

    stashed: WIP

--- resources/js/Components/Molecules/ListInput.vue
+++ resources/js/Components/Molecules/ListInput.vue
@@ -3,6 +3,10 @@
         <div class="list-input__inner">
             <h3 class="list-input__heading">Searched Cities</h3>
             <div class="list-input__main">
+                <Select
+                    :optionLabels="optionLabels"
+                    :selectButtonText="selectButtonText"
+                ></Select>
                 <ul class="list-input__list">
                     <li
                         class="list-input__item"
@@ -18,8 +22,17 @@
 
 <script setup>
 import Icon from "@/Components/Atoms/Icon.vue";
+import Select from "@/Components/Molecules/Select.vue";
 
 const props = defineProps({
     searchInputs: Array,
+    optionLabels: {
+        type: Array,
+        default: ["newest", "oldest"],
+    },
+    selectButtonText: {
+        type: String,
+        default: "sort by",
+    },
 });
 </script>
---
+++ resources/js/Components/Molecules/Select.vue
@@ -0,0 +1,38 @@
+<template>
+    <select class="select" name="select-custom" id="select">
+        <button class="select__button" type="button">
+            <span>{{ selectButtonText }}</span>
+            <selectedcontent> </selectedcontent>
+        </button>
+        <option
+            class="select__option"
+            value="newest"
+            v-for="label in optionLabels"
+        >
+            <div class="custom-option">
+                <span class="option-text">{{ label }}</span>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 40 40"
+                    aria-hidden="true"
+                    class="custom-option__icon"
+                >
+                    <path
+                        fill="currentColor"
+                        d="M37.856 12.91 20 29.717 2.144 12.91l1.712-1.82L20 26.283 36.144                            11.09l1.712 1.82Z"
+                    ></path>
+                </svg>
+            </div>
+        </option>
+    </select>
+</template>
+
+<script setup>
+import Icon from "@/Components/Atoms/Icon.vue";
+
+const props = defineProps({
+    optionLabels: Array,
+    selectButtonText: String,
+});
+</script>
